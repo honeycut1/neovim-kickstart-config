854f3f08047b806332a219d2312f7402d73ec7cc
J.Honeycutt <honeycutt7483@gmail.com>
2025-07-23 19:13:45 -0400

    tokyonight color theme and other tweeks

--- init.lua
+++ init.lua
@@ -16,14 +16,14 @@ vim.opt.rtp:prepend(lazypath)
 -- Set up plugins
 require('lazy').setup {
   require 'plugins.neotree',
-  require 'plugins.colortheme',
+  require 'plugins.colortheme-tokyonight',
   require 'plugins.bufferline',
   require 'plugins.lualine',
   require 'plugins.treesitter',
   require 'plugins.telescope',
-  require 'plugins.lsp',
+  -- require 'plugins.lsp',
   require 'plugins.autocompletion',
-  require 'plugins.none-ls',
+  -- require 'plugins.none-ls',
   require 'plugins.gitsigns',
   require 'plugins.alpha',
   require 'plugins.indent-blankline',
