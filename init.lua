5e7974924fa2ee4e3bc882703702d7305bc75adb
J.Honeycutt <john.honeycutt@xyleminc.com>
2025-07-28 19:28:00 -0400

    add jenkinsfile syntax support

--- init.lua
+++ init.lua
@@ -29,7 +29,15 @@ require('lazy').setup {
   require 'plugins.indent-blankline',
   require 'plugins.misc',
   require 'plugins.comment',
+
 }
+
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+  pattern = {"Jenkinsfile", "Jenkinsfile-*", "Jenkinsfile.*"},
+  callback = function()
+    vim.bo.filetype = "groovy"
+  end,
+})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
